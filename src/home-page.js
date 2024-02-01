a6c3a16f547e556bbae397318447b32be506b388
astrosuka <yegresvoselok@gmail.com>
2024-02-01 10:15:18 -0300

    Add home page module

---
+++ src/home-page.js
@@ -0,0 +1,16 @@
+export default function homePage() {
+    const content = document.querySelector('#content');
+    
+    const image = document.createElement('img');
+    image.setAttribute('src', 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+    image.setAttribute('width', '800');
+    image.setAttribute('alt', 'a dish with fresh vegetables');
+    
+    const title = document.createElement('h1');
+    title.textContent = 'My Restaurant';
+
+    const description = document.createElement('p');
+    description.textContent = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde deleniti reprehenderit a similique qui iste reiciendis quis voluptatem velit! Inventore illo rerum libero debitis alias assumenda omnis et corrupti veritatis.';
+
+    content.append(image, title, description);
+}
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import homePage from "./homeE-page";
+
+homePage();
